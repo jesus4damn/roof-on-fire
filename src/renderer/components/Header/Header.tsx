51713746e879990300833f37bfc94727b11e6fca
Viktor.Savitski <Viktor.Savitski@coxautoinc.com>
2020-04-28 12:43:51 +0300

    add file format

--- src/renderer/components/Header/Header.tsx
+++ src/renderer/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {connect} from "react-redux";
+import ExcelReader from "../../../data-helper/ExcelReader";
 
 require('./Header.scss');
 
@@ -11,11 +12,15 @@ interface IProps {
 
 const Header:React.FC<IProps> = ({resetData, loadData, saveData}) => {
     return (
+        <>
         <div className={'headerContent'}>
             <button onClick={resetData}>resetState</button>
             <button onClick={loadData}>loadData</button>
-            <button onClick={saveData}>saveData</button>
+            <button onClick={saveData}>saveData</button>          
         </div>
+        
+         <ExcelReader/>
+         </>
     )
 };
 
