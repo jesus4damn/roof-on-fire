afc4c792dd01093908e81a573800191fa24b507e
Aleh <c1iff6ooth@protonmail.com>
2020-05-20 01:49:29 +0300

    webpack config
    webpack-node-externals removed, webpack.base & main config updated

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     mode: 'development',
@@ -18,17 +17,5 @@ module.exports = {
     },
     devtool: 'source-map',
     plugins: [
-    ],
-    target: 'node',
-    externals: [nodeExternals({
-        modulesFromFile: true,
-        whitelist: [
-            /hot/,
-            /^lodash/, /babel/,
-            /^core-js/, /react/,
-            /redux/, /wavesurfer/,
-            /^uuid/, 'reselect', 'xlsx', /webpack/,
-            'uws', 'dmx'
-        ]
-    })]
+    ]
 };
