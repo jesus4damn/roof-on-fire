0b266805254acbbdeef46e3865e2f4b53f982b36
Victor_Savitsky <victors@unl.solutions>
2020-07-07 15:44:53 +0300

    fix routes

--- src/renderer/components/API/API.ts
+++ src/renderer/components/API/API.ts
@@ -5,11 +5,11 @@ const route = "http://localhost:5000";
 export const controllerAPI = {
     sendEvent: async (event: string) => {
         console.log(event);
-        const res = await axios.post(route, event);
+        const res = await axios.post(`${route}/dmx/${event}`, event);
         console.log(res);
     },
     sendVal: async (val: {channel: number , value: number}) => {
-        const res = await axios.post(route, val);
+        const res = await axios.post(`${route}/dmx/set`, val);
         console.log(res);
         return "AAAAAA";
     },
